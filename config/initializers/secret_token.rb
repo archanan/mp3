9826273edcd8d6f8f1fd199bb8de19cbfa9741f7
User <user@user.(none)>
2013-08-27 09:55:43 +0700

    commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectMp3::Application.config.secret_key_base = '38be8487edfccc9f7e858858b24b04e52b1f5a9274c9291ec32ae7da00309019b31a51f92e5b151d72fea45292f22c2dda0752ca5e1fad9ebd0ae7a892142239'
